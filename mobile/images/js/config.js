d88951772e569dbbf8693c27f8e4f875b509ee97
smartidea7 <smartidea7777@gmail.com>
2026-07-18 01:58:49 +0330

    Add files via upload

---
+++ mobile/images/js/config.js
@@ -0,0 +1,53 @@
+
+/*==================================================
+    Categories
+==================================================*/
+
+const productCategories=[
+
+{
+id:"consumables",
+title:"تجهیزات پزشکی مصرفی",
+en:"Medical Consumables",
+icon:"icon-consumables",
+folder:"consumables",
+count:3
+},
+
+{
+id:"hospital",
+title:"ترالی و تجهیزات هتلینگ بیمارستانی",
+en:"Hospital Equipment",
+icon:"icon-hospital",
+folder:"hospital",
+count:2
+},
+
+{
+id:"gas",
+title:"کنسول و تجهیزات گازهای طبی",
+en:"Medical Gas Systems",
+icon:"icon-gas",
+folder:"gas",
+count:2
+},
+
+{
+id:"lead",
+title:"سرب و تجهیزات حفاظتی",
+en:"Lead Shielding Materials",
+icon:"icon-lead",
+folder:"lead",
+count:2
+},
+
+{
+id:"special",
+title:"سایر کالاهای تخصصی",
+en:"Specialized Products",
+icon:"icon-special",
+folder:"special",
+count:0
+}
+
+];
